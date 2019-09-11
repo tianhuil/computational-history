374a7a9dba9221d0196502bdb6b8b483342dc173
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2019-09-11 20:27:13 +0000

    First attempt at constitution

---
+++ src/Constitution.scala
@@ -0,0 +1,53 @@
+val articles = List(
+    "article one",
+    "article two",
+    "article three",
+    "article four",
+    "article five",
+    "article six",
+    "article seven"
+)
+
+val amendments = List(
+    "first amendment",
+    "second amendment",
+    "third amendment",
+    "fourth amendment",
+    "fifth amendment",
+    "sixth amendment",
+    "seventh amendment",
+    "eighth amendment",
+    "ninth amendment",
+    "tenth amendment",
+    "eleventh amendment",
+    "twelfth amendment",
+    "thirteenth amendment",
+    "fourteenth amendment",
+    "fifteenth amendment",
+    "sixteenth amendment",
+    "seventeenth amendment",
+    "eighteenth amendment",
+    "nineteenth amendment",
+    "twentieth amendment",
+    "twenty-first amendment",
+    "twenty-second amendment",
+    "twenty-third amendment",
+    "twenty-fourth amendment",
+    "twenty-fifth amendment",
+    "twenty-sixth amendment",
+    "twenty-seventh amendment"
+)
+
+val queries = articles ++ amendments
+
+val inputFiles = Array(
+    "/mnt/volume_sfo2_02/downloads/google_ngrams/5/googlebooks-eng-us-all-5gram-20120701-ar.gz",
+    "/mnt/volume_sfo2_02/downloads/google_ngrams/5/googlebooks-eng-us-all-5gram-20120701-ei.gz",
+    "/mnt/volume_sfo2_02/downloads/google_ngrams/5/googlebooks-eng-us-all-5gram-20120701-ni.gz",
+).mkString(",")
+
+val data = spark.read.textFile(inputFiles)
+
+(data.filter(line => queries.exists(q => line.toLowerCase().contains(q)))
+    .repartition(5)
+    .saveAsTextFile("/mnt/volume_sfo2_03/downloads/google_ngrams/5-part/constitution-sample"))
