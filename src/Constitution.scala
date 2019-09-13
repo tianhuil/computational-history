c0650db6fdd8f7fd15178a89327af84f1cb67781
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2019-09-13 00:46:05 +0000

    Run full constitution job

--- src/Constitution.scala
+++ src/Constitution.scala
@@ -42,14 +42,8 @@ val amendments = List(
 
 val queries = articles ++ amendments
 
-val inputFiles = Seq(
-    "/mnt/volume_sfo2_02/downloads/google_ngrams/5/googlebooks-eng-us-all-5gram-20120701-ar.gz",
-    "/mnt/volume_sfo2_02/downloads/google_ngrams/5/googlebooks-eng-us-all-5gram-20120701-ei.gz",
-    "/mnt/volume_sfo2_02/downloads/google_ngrams/5/googlebooks-eng-us-all-5gram-20120701-ni.gz"
-)
-
-val data = spark.sparkContext.union(inputFiles.map(file => spark.read.textFile(file).rdd))
+val data = spark.read.textFile("/mnt/volume_sfo2_03/downloads/google_ngrams/5-part/*.gz").rdd
 
 (data.filter(line => queries.exists(q => line.toLowerCase().contains(q)))
-    .repartition(1)
-    .saveAsTextFile("/mnt/volume_sfo2_03/downloads/google_ngrams/5-part/constitution-sample-gz", classOf[GzipCodec]))
+    .repartition(10)
+    .saveAsTextFile("/mnt/volume_sfo2_03/downloads/google_ngrams/5/constitution-gz", classOf[GzipCodec]))
