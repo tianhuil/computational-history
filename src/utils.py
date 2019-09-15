f50e211998c90939fa3ea6009ce5c24ccbf25368
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2019-09-15 21:02:47 +0000

    More work on constitution

--- src/utils.py
+++ src/utils.py
@@ -5,10 +5,17 @@ from glob import glob
 from dask.delayed import delayed
 import dask.dataframe as dd
 import pandas as pd
+import csv
 
 def read_ngrams(glob_pattern):
-  columns = ['ngram', 'year', 'total', 'distinct']
-  return dd.read_csv(glob_pattern, sep='\t', blocksize=None, compression='gzip', header=None, names=columns)
+  return dd.read_csv(
+    glob_pattern,
+    sep='\t',
+    blocksize=None,
+    compression='gzip',
+    header=None,
+    quoting=csv.QUOTE_NONE
+  )
 
 def save_ngrams(df, output_dir):
   dfs = df.to_delayed()
